f7a592f57d2b28a8aad54fe85dbb5d8a945ccbaa
Abdullah_Gumi <abdulgumi77@gmail.com>
2021-01-01 11:34:35 +0100

    finished exercise 10.8

---
+++ src/components/SignInForm.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Text, TouchableWithoutFeedback, View, StyleSheet } from 'react-native';
+import FormikTextInput from './FormikTextInput';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    backgroundColor: '#ffffff',
+    padding: 15,
+    margin: 10,
+  },
+  input: {
+      height:45,
+      marginLeft:16,
+      padding: 15,
+      borderWidth: 1,
+      borderColor: '#eee',
+      borderRadius: 4,
+      margin: 10
+  },
+  siginBtn: {
+    paddingTop: 10,
+    height:45,
+    backgroundColor: theme.colors.primary,
+    borderRadius: 4,
+  }
+})
+
+const SignInForm = ({ onSubmit }) => {
+  return (
+    <View style={styles.inputContainer}>
+      <FormikTextInput
+        style={styles.input}
+        name="username"
+        placeholder="username"
+      />
+      <FormikTextInput
+        style={styles.input}
+        name="password"
+        placeholder="password"
+        secureTextEntry
+      />
+      <TouchableWithoutFeedback onPress={onSubmit}>
+      <View style={styles.siginBtn}>
+          <Text style={{ color: 'white', alignSelf: 'center'}}>Sign In</Text>
+      </View>
+      </TouchableWithoutFeedback>
+    </View>
+  );
+};
+
+export default SignInForm;
