9e7623d60ad5ec9b7d15870394fc4cf15ff1ed8a
Abdullah_Gumi <abdulgumi77@gmail.com>
2021-01-05 15:17:39 +0100

    finished exercise 10.14

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,13 +1,17 @@
 import { useMutation } from '@apollo/react-hooks';
 
 import { AUTHORIZE } from '../graphql/mutation';
-
+import AuthStorage from '../utils/authStorage';
 
 const useSignIn = () => {
   const [mutate, result] = useMutation(AUTHORIZE);
+  const auth = new AuthStorage('auth');
 
   const signIn = async ({ username, password }) => {
   	mutate({ variables: { username, password }})
+    await auth.setAccessToken(result.data.authorize.accessToken);
+    const token = await auth.getAccessToken();
+    console.log('token', token);  	
   	return result;
   };
 
